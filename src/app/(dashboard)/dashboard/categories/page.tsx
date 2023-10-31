a7064e3e82c136875749423c999ab65e59d0b874
Jorge Almonacid <jorgealmonacid24@gmail.com>
2023-10-31 15:54:08 -0300

    Refactoring

--- src/app/(dashboard)/dashboard/categories/page.tsx
+++ src/app/(dashboard)/dashboard/categories/page.tsx
@@ -6,9 +6,10 @@ import Actions from "@/components/dashboard/Actions"
 import { getData } from "@/utils/services"
 import { useQuery } from "@tanstack/react-query"
 import { queryKeys } from "@/utils/consts"
+import Wrapper from "@/components/dashboard/Wrapper"
 
 const Categories = () => {
-  const { isLoading, data: categories} = useQuery({
+  const { isLoading, data } = useQuery({
     queryKey: [queryKeys.GET_CATEGORIES],
     queryFn: () => {
       return getData('categories');
@@ -21,44 +22,35 @@ const Categories = () => {
   }
 
   return (
-    <div className="p-4 h-[800px] overflow-scroll">
-      <div className="flex justify-between">
-        <h1 className="text-2xl text-stone-600 font-semibold">Categorías</h1>
-        <Link href="/dashboard/categories/add">
-          <button className="text-sm py-2 px-4 bg-red-500 text-white font-semibold rounded-md">Agregar</button>
-        </Link>
-      </div>
-      <div className="h-[2px] w-full bg-stone-100 my-4"></div>
-      <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">Nombre</th>
-                    <th scope="col" className="px-6 py-3">Total de productos</th>
-                    <th scope="col" className="px-6 py-3">Acciones</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {
-                    categories.map((category: CategoryWithProducts) => (
-                      <tr className="bg-white border-b hover:bg-gray-100 cursor-pointer" key={category.id}>
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{category.title}</th>
-                        <td className="px-6 py-4">{category._count.products}</td>
-                        <td className="px-6 py-4">
-                          <Actions
-                            remove
-                            edit
-                          />
-                        </td>
-                      </tr>
-                    ))
-                  }
-              </tbody>
-          </table>
-      </div>
-
-    </div>
+    <Wrapper title="Categorías" url="/categories/add">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" className="px-6 py-3">Nombre</th>
+            <th scope="col" className="px-6 py-3">Total de productos</th>
+            <th scope="col" className="px-6 py-3">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+        data.map((category: CategoryWithProducts) => (
+          <tr className="bg-white border-b hover:bg-gray-100 cursor-pointer" key={category.id}>
+            <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{category.title}</td>
+            <td className="px-6 py-4">{category._count.products}</td>
+            <td className="px-6 py-4">
+              <Actions
+                remove
+                edit
+              />
+            </td>
+          </tr>
+        ))
+        }
+        </tbody>
+      </table>
+    </Wrapper>
   )
 }
+
 
 export default Categories
--- src/app/(dashboard)/dashboard/products/page.tsx
+++ src/app/(dashboard)/dashboard/products/page.tsx
@@ -7,9 +7,10 @@ import ToggleStatus from "@/components/dashboard/ToggleStatus"
 import Actions from "@/components/dashboard/Actions"
 import { queryKeys } from "@/utils/consts"
 import { getData } from "@/utils/services"
+import Wrapper from "@/components/dashboard/Wrapper"
 
 const Dashboard = () => {
-  const { isLoading, data: products} = useQuery({
+  const { isLoading, data} = useQuery({
     queryKey: [queryKeys.GET_PRODUCTS],
     queryFn: () => {
       return getData('products');
@@ -22,46 +23,38 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="p-4 h-[800px] overflow-scroll">
-      <div className="flex justify-between">
-        <h1 className="text-2xl text-stone-600 font-semibold">Productos</h1>
-        <button className="text-sm py-2 px-4 bg-red-500 text-white font-semibold rounded-md">Agregar</button>
-      </div>
-      <div className="h-[2px] w-full bg-stone-100 my-4"></div>
-      <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">Nombre</th>
-                    <th scope="col" className="px-6 py-3">Categoría</th>
-                    <th scope="col" className="px-6 py-3">Precio</th>
-                    <th scope="col" className="px-6 py-3">Estado</th>
-                    <th scope="col" className="px-6 py-3">Acciones</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {
-                    products.map((product: ProductWithCategory) => (
-                      <tr className="bg-white border-b hover:bg-gray-100 cursor-pointer" key={product.id}>
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{product.title}</th>
-                        <td className="px-6 py-4">{product.category.title}</td>
-                        <td className="px-6 py-4">{priceFormatter(product.price)}</td>
-                        <td className="px-6 py-4"><ToggleStatus status={product.active}/></td>
-                        <td className="px-6 py-4">
-                          <Actions
-                            view
-                            remove
-                            edit
-                          />
-                        </td>
-                      </tr>
-                    ))
-                  }
-              </tbody>
-          </table>
-      </div>
-
-    </div>
+    <Wrapper title="Productos" url="/products/add">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" className="px-6 py-3">Nombre</th>
+            <th scope="col" className="px-6 py-3">Categoría</th>
+            <th scope="col" className="px-6 py-3">Precio</th>
+            <th scope="col" className="px-6 py-3">Estado</th>
+            <th scope="col" className="px-6 py-3">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((product: ProductWithCategory) => (
+              <tr className="bg-white border-b hover:bg-gray-100 cursor-pointer" key={product.id}>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{product.title}</th>
+                <td className="px-6 py-4">{product.category.title}</td>
+                <td className="px-6 py-4">{priceFormatter(product.price)}</td>
+                <td className="px-6 py-4"><ToggleStatus status={product.active}/></td>
+                <td className="px-6 py-4">
+                  <Actions
+                    view
+                    remove
+                    edit
+                  />
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </Wrapper>
   )
 }
 
