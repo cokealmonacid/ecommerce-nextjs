008bcc64a49bd6216e29ee435a3398d20e65ffce
Jorge Almonacid <jorgealmonacid24@gmail.com>
2023-11-02 10:42:52 -0300

    Added react-hooks-form

--- src/app/(dashboard)/dashboard/slider/add/page.tsx
+++ src/app/(dashboard)/dashboard/slider/add/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image"
 import { useState } from "react"
 import { toast } from 'react-toastify'
 import { useMutation, useQueryClient } from "@tanstack/react-query"
